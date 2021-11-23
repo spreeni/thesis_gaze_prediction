580d6abd9369b6152031bf5bad4855b22c086656
Yannic Spreen-Ledebur <yannicsl@risha.ni.tu-berlin.de>
2021-11-23 13:34:57 +0100

    Added sampling option to write to file instead ofplotting; included predicted em data

--- sample_model.py
+++ sample_model.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 from pytorchvideo.data import make_clip_sampler
 
 import utils
@@ -7,9 +8,11 @@ from gaze_video_data_module import VAL_TRANSFORM
 from model import GazePredictionLightningModule
 
 
-_DATA_PATH = r'data/GazeCom/movies_m2t_224x224'
-_CHECKPOINT_PATH = r'lightning_logs_ssh/version_39/checkpoints/epoch=9-step=399.ckpt'
-_CLIP_DURATION = 2
+_PLOT_RESULTS = False
+
+_DATA_PATH = r'data/GazeCom/movies_m2t_224x224/val'
+_CHECKPOINT_PATH = r'lightning_logs/version_42/checkpoints/epoch=9-step=798.ckpt'
+_CLIP_DURATION = 5
 _VIDEO_SUFFIX = ''
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -27,11 +30,10 @@ dataset = gaze_labeled_video_dataset(
     decoder="pyav",
 )
 
-model = GazePredictionLightningModule.load_from_checkpoint(_CHECKPOINT_PATH)
-
+model = GazePredictionLightningModule.load_from_checkpoint(_CHECKPOINT_PATH).to(device=device)
 
 samples = 4
-for _ in range(samples):
+for i in range(samples):
     sample = next(dataset)
 
     y_hat = model(sample['video'][None, :].to(device=device))[0]
@@ -40,5 +42,18 @@ for _ in range(samples):
     # (C, F, H, W) -> (F, H, W, C)
     frames = torch.swapaxes(sample['video'].T, 0, 2)
 
-    utils.plot_frames_with_labels(frames.cpu().detach().numpy(), y_hat.cpu().detach().numpy(),
-                                  sample['em_data'].cpu().detach().numpy(), y[:, None, :].tolist(), box_width=8)
+    frames = frames.cpu().detach().numpy()
+    em_data_hat = None
+    y_hat = y_hat.cpu().detach().numpy()
+    em_data = sample['em_data'].cpu().detach().numpy()
+    y = y[:, None, :].tolist()
+
+    if y_hat.shape[1] == 3:
+        em_data_hat = y_hat[:, 2]
+        y_hat = y_hat[:, :2]
+
+    if _PLOT_RESULTS:
+        utils.plot_frames_with_labels(frames, y_hat, em_data_hat, y, em_data, box_width=8)
+    else:
+        filepath = f'data/sample_outputs/version_42_{i}'
+        np.savez(filepath, frames=frames, em_data_hat=em_data_hat, y_hat=y_hat, em_data=em_data, y=y)
