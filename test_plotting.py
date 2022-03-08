5e68dd9b27b5db305ea21416efb6c8b8121b51e9
Yannic Spreen-Ledebur <yannic.sl@gmx.de>
2022-03-08 11:09:20 +0100

    Small changes in plotting paths

--- test_plotting.py
+++ test_plotting.py
@@ -1,10 +1,10 @@
 import utils
 
-VIDEO = 'breite_strasse'
-OBSERVER = 'ALK'
+VIDEO = 'holsten_gate'
+OBSERVER = 'AAW' # AAF
 
-video_path = f'data/GazeCom/movies_mpg/{VIDEO}.mpg'
-label_path = f'data/GazeCom/movies_mpg_frames/train/label_data/{VIDEO}/{OBSERVER}_{VIDEO}.txt'
+video_path = f'data/GazeCom/movies_m2t/{VIDEO}.m2t'
+label_path = f'data/GazeCom/deepEM_classifier/ground_truth_framewise/{VIDEO}/{OBSERVER}_{VIDEO}.txt'
 dir_arff_ground_truth = r'data/GazeCom/deepEM_classifier/ground_truth'
 raw_label_path = f'{dir_arff_ground_truth}/{VIDEO}/{OBSERVER}_{VIDEO}.arff'
 
