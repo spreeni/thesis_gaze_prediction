773a55ec84c0aaf6903fdae66e9c91375f400331
Yannic Spreen-Ledebur <yannic.sl@gmx.de>
2022-04-06 17:02:57 +0200

    Save NSS in sample logs

--- sample_model.py
+++ sample_model.py
@@ -18,8 +18,8 @@ _OUTPUT_DIR = r"data/sample_outputs/version_305"
 _MODE = 'train'
 
 _DATA_PATH = f'data/GazeCom/movies_m2t_224x224/{_MODE}'
-#_DATA_PATH = f'data/GazeCom/movies_m2t_224x224/single_video/{_MODE}'
-#_DATA_PATH = f'data/GazeCom/movies_m2t_224x224/single_clip/{_MODE}'
+_DATA_PATH = f'data/GazeCom/movies_m2t_224x224/single_video/{_MODE}'
+_DATA_PATH = f'data/GazeCom/movies_m2t_224x224/single_clip/{_MODE}'
 
 _CHECKPOINT_PATH = r'data/lightning_logs/version_305/checkpoints/epoch=30-step=1868.ckpt'
 
@@ -102,14 +102,17 @@ for i in range(0, samples):
 
     nss_orig = score_gaussian_density(video_name, y[0, :, :], frame_ids=frame_indices)
     nss = score_gaussian_density(video_name, y_hat.astype(int), frame_ids=frame_indices)
+    gaze_mid = np.ones(y_hat.shape, dtype=np.int32) * 112
+    nss_mid = score_gaussian_density(video_name, gaze_mid, frame_ids=frame_indices)
     print("NSS original clip:", nss_orig)
-    print("NSS prediction:", nss, "\n")
+    print("NSS prediction:", nss)
+    print("NSS middle baseline:", nss_mid, "\n")
 
     utils.plot_frames_with_labels(frames, y_hat, em_data_hat, y, em_data, box_width=8, save_to_directory=save_dir)
     subprocess.call(f"/mnt/antares_raid/home/yannicsl/miniconda3/envs/thesis/bin/ffmpeg -framerate 10 -start_number 0 -i {i}/%03d.png -pix_fmt yuv420p {_MODE}_{i}.mp4", cwd=_OUTPUT_DIR, shell=True)
     shutil.rmtree(save_dir)
     with open(os.path.join(_OUTPUT_DIR, "metadata.txt", "a")) as f:
-        f.write(f"{_MODE}_{i}: {video_name}+{observer}, Frames {frame_indices[0]}-{frame_indices[-1]}\n")
+        f.write(f"{_MODE}_{i}: {video_name}+{observer}, Frames {frame_indices[0]}-{frame_indices[-1]}, nss (original, prediction, middle): ({nss_orig:.2f}, {nss:.2f}, {nss_mid:.2f})\n")
     #if _PLOT_RESULTS:
     #    utils.plot_frames_with_labels(frames, y_hat, em_data_hat, y, em_data, box_width=8)
     #else:
