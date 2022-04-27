5a65160c395bf8128f63ee0ef4e2ecf7ecb70977
Yannic Spreen-Ledebur <yannic.sl@gmx.de>
2022-04-27 09:22:45 +0200

    Bugfix for LSTM as recurrent model

--- model.py
+++ model.py
@@ -79,7 +79,7 @@ class GazePredictionLightningModule(pytorch_lightning.LightningModule):
         if self.mode == 'LSTM':
             if n_teacher_vals > 0:
                 n_features_lstm = n_features + n_teacher_vals * self.out_features
-            self.lstm = torch.nn.LSTM(input_size=n_features, hidden_size=rim_hidden_size, device=device, bidirectional=False)
+            self.lstm = torch.nn.LSTM(input_size=n_features_lstm, hidden_size=rim_hidden_size, device=device, bidirectional=False)
         else:
             self.rim = RIM(
                 device=device,
@@ -106,7 +106,7 @@ class GazePredictionLightningModule(pytorch_lightning.LightningModule):
         inp = torch.randn(frames, self.batch_size, 1 if not self.channel_wise_attention else out_channels, n_features, device=device)
         with torch.no_grad():
             if self.mode == 'LSTM':
-                inp = inp.reshape(frames, self.batch_size, n_features)
+                inp = torch.randn(frames, self.batch_size, n_features_lstm, device=device)
                 out, _ = self.lstm(inp)
             else:
                 out, _, _ = self.rim(inp)
