3ca0ef9bf0cb3e011b9312cf8539c88cbbf2ae74
Yannic Spreen-Ledebur <yannic.sl@gmx.de>
2022-05-10 09:38:03 +0200

    Added output as video to test_plotting.py

--- test_plotting.py
+++ test_plotting.py
@@ -8,6 +8,9 @@ label_path = f'data/GazeCom/deepEM_classifier/ground_truth_framewise/{VIDEO}/{OB
 dir_arff_ground_truth = r'data/GazeCom/deepEM_classifier/ground_truth'
 raw_label_path = f'{dir_arff_ground_truth}/{VIDEO}/{OBSERVER}_{VIDEO}.arff'
 
-out_path = f'data/GazeCom/movies_with_groundtruth/{OBSERVER}_{VIDEO}'
+out_path = f'data/GazeCom/movies_with_groundtruth'
+out_path_frames = f'{out_path}/{OBSERVER}_{VIDEO}'
 
-utils.plot_gazecom_frames_with_labels(video_path, label_path, raw_label_path, save_to_directory=out_path)
+utils.plot_gazecom_frames_with_labels(video_path, label_path, raw_label_path, save_to_directory=out_path_frames)
+if out_path is not None:
+    utils.create_movie_from_frames(out_path, f'{OBSERVER}_{VIDEO}', f'{OBSERVER}_{VIDEO}.mp4', fps=10, width_px=1280)
