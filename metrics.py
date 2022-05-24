9dc88581bfd004a7d00e882c818f99d1e5ccd1da
Yannic Spreen-Ledebur <yannic.sl@gmx.de>
2022-05-24 23:33:28 +0200

    Normalize histogram results before comparing change metrics

--- metrics.py
+++ metrics.py
@@ -22,11 +22,19 @@ def calc_similarity_gaze_change_distance(change_len1, change_len2, metric):
     counts_len1, _ = np.histogram(change_len1, bins=np.linspace(0, 2., 100))
     counts_len2, _ = np.histogram(change_len2, bins=np.linspace(0, 2., 100))
 
+    # Normalize to probability density
+    counts_len1 /= len(change_len1)
+    counts_len2 /= len(change_len2)
+
     return calc_similarity(counts_len1, counts_len2, metric)
 
 
 def calc_similarity_gaze_change_orientation(change_deg1, change_deg2, metric):
     counts_deg1, _ = np.histogram(change_deg1, bins=np.linspace(0, 360, 100))
     counts_deg2, _ = np.histogram(change_deg2, bins=np.linspace(0, 360, 100))
+
+    # Normalize to probability density
+    counts_deg1 /= len(change_deg1)
+    counts_deg2 /= len(change_deg2)
 
     return calc_similarity(counts_deg1, counts_deg2, metric)
